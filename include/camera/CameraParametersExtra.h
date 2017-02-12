b104242d7aa640bf0abc73365be5d6115ec89837
Okeys <Olegrus1100@gmail.com>
2017-02-12 20:52:55 +0530

    Fix cam and compiling error
    Change-Id: I68821e6a549e58bf768cf62259e6215f6de6c1a4

--- include/camera/CameraParametersExtra.h
+++ include/camera/CameraParametersExtra.h
@@ -13,11 +13,3 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-#define CAMERA_PARAMETERS_EXTRA_C \
-const char CameraParameters::FOCUS_MODE_MANUAL_POSITION[] = "manual"; \
-const char CameraParameters::WHITE_BALANCE_MANUAL_CCT[] = "manual-cct";
-
-#define CAMERA_PARAMETERS_EXTRA_H \
-    static const char FOCUS_MODE_MANUAL_POSITION[]; \
-    static const char WHITE_BALANCE_MANUAL_CCT[];
